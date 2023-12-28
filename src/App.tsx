5dca3b57c95a9580d38bd6b02c119297fea3bf70
AlfarAbu <raflasalfa@gmail.com>
2023-12-28 22:45:07 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './globals.css';
+import { Routes, Route } from 'react-router-dom';
+//import React from 'react'
+
+
+const App = () => {
+  return (
+ <z z   main>
+  )
+}
+
+export default App
